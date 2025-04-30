c69fef53f6d2252f524448feb8eb4811d4c4ab67
Nguyen Hoang <hoangducnguyen3@gmail.com>
2025-04-30 17:10:04 +0700

    Add main.go as application entry point

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hoangNguyenDev3/redis-clone/cmd"
+)
+
+func main() {
+	// Set up signal handling for graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
